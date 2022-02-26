dae04aeb2444ad23323f17ee67d1e6d390bcf3f9
mmalikhidayatulloh <m4likhidayatulloh@gmail.com>
2022-02-26 07:49:45 +0700

    permulaan

---
+++ code/keliling_dan_luas.cpp
@@ -0,0 +1,9 @@
+# include<iostream>
+using namespace std;
+int main(){
+    double pi=3.14;
+    double r=4.0;
+
+    cout << "Keliling= " << 2.0*pi*r << "\n";
+    cout << "Luas= " << pi*r*r << "\n";
+}
